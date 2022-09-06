1a5f6f34363f880ef03347d64e4ca029d2d4badd
Viv Lim <git@vvn.space>
2022-09-05 18:50:17 -0700

    make launch script usable from other places

--- launch.sh
+++ launch.sh
@@ -1,3 +1,5 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-MYVIMRC=./init.vim nvim -u ./init.vim --cmd ":set runtimepath+=`pwd`"
+SCRIPT_DIR="$( dirname -- "$0"; )"
+
+MYVIMRC=$SCRIPT_DIR/init.vim nvim -u $SCRIPT_DIR/init.vim --cmd ":set runtimepath+=$SCRIPT_DIR" $@
