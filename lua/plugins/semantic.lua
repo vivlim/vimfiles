9090d7013e8a48656c5ce09ea77fe3c37bee4419
Viv Lim <git@vvn.space>
2023-12-19 03:02:42 -0800

    enable lua lsp

--- lua/plugins/semantic.lua
+++ lua/plugins/semantic.lua
@@ -56,11 +56,43 @@ return {
             capabilities = capabilities,
             cmd = { "elixir-ls" }, -- This should be on the path if the project has a nix flake devshell & direnv configured.
         }
+        lsp.lua_ls.setup {
+            on_attach = on_attach,
+            capabilities = capabilities,
+            on_init = function(client)
+                local path = client.workspace_folders[1].name
+                if not vim.loop.fs_stat(path..'/.luarc.json') and not vim.loop.fs_stat(path..'/.luarc.jsonc') then
+                client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
+                    Lua = {
+                    runtime = {
+                        -- Tell the language server which version of Lua you're using
+                        -- (most likely LuaJIT in the case of Neovim)
+                        version = 'LuaJIT'
+                    },
+                    -- Make the server aware of Neovim runtime files
+                    workspace = {
+                        checkThirdParty = false,
+                        library = {
+                        vim.env.VIMRUNTIME
+                        -- "${3rd}/luv/library"
+                        -- "${3rd}/busted/library",
+                        }
+                        -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+                        -- library = vim.api.nvim_get_runtime_file("", true)
+                    }
+                    }
+                })
+
+                client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+                end
+                return true
+            end
+        }
     end,
 },
 {"nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    config = function () 
+    config = function ()
       local configs = require("nvim-treesitter.configs")
 
       configs.setup({
