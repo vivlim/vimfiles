9bcda8593a54961dbf1a68ff3359ad67bda9b9bd
Viv Lim <git@vvn.space>
2023-12-18 19:45:43 -0800

    Actually, use the default `K` binding for the hover popup, in case that synergizes with stuff I don't usually use yet

--- lua/plugins/semantic.lua
+++ lua/plugins/semantic.lua
@@ -24,9 +24,9 @@ return {
             local bufopts = { noremap=true, silent=true, buffer=bufnr }
             vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-            vim.keymap.set('n', '<space>h', vim.lsp.buf.hover, bufopts)
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
             vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-            vim.keymap.set('n', '<space>H', vim.lsp.buf.signature_help, bufopts)
+            vim.keymap.set('n', '<space>h', vim.lsp.buf.signature_help, bufopts)
             vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
             vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
             vim.keymap.set('n', '<space>wl', function()
@@ -64,7 +64,7 @@ return {
       local configs = require("nvim-treesitter.configs")
 
       configs.setup({
-          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "typescript", "html", "rust" },
+          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "typescript", "html", "rust", "nix" },
           sync_install = false,
           highlight = { enable = true },
           indent = { enable = true },  
