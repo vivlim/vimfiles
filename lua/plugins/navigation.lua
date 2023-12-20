26091407d1b2697c195914e966b9109c9021e22a
Viv Lim <git@vvn.space>
2023-12-19 17:51:49 -0800

    rework telescope init

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -1,15 +1,23 @@
 return {
     { "nvim-telescope/telescope.nvim",
         tag = '0.1.5',
-        dependencies = { 'nvim-lua/plenary.nvim' },
-        opts = {
-            defaults = {
-                layout_strategy = 'vertical',
-                layout_config = {
-                    vertical = {width = 0.8}
-                },
-            },
+        dependencies = {
+            'nvim-telescope/telescope-fzf-native.nvim',
+            'nvim-lua/plenary.nvim',
         },
+        init = function()
+            local telescope = require("telescope")
+            telescope.setup {
+                defaults = {
+                    layout_strategy = 'vertical',
+                    layout_config = {
+                        vertical = {width = 0.8}
+                    },
+                },
+                extensions = {
+                },
+            }
+        end,
     },
     { 'nvim-telescope/telescope-fzf-native.nvim',
         build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
