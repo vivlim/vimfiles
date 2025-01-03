f049d3dc0321c9277785e503ff2a33009c94d72f
Viv <git@vvn.space>
2025-01-02 18:57:15 -0800

    don't notify about which-key warnings

--- lua/plugins/bindings.lua
+++ lua/plugins/bindings.lua
@@ -7,9 +7,7 @@ return {
             local ts = require("telescope.builtin")
 
             wk.setup({
-                -- your configuration comes here
-                -- or leave it empty to use the default settings
-                -- refer to the configuration section below
+                notify = false, -- don't warn about issues with mappings; use :checkhealth which-key manually
             })
             wk.register({
                 f = {
