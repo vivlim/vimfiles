4325dfb6708ff7934a50286f1cdd2e44cb63109c
Viv Lim <git@vvn.space>
2025-01-05 14:56:36 -0800

    attempt to view images as sixels

--- lua/plugins/images.lua
+++ lua/plugins/images.lua
@@ -1,12 +1,18 @@
 return {
-    "HakonHarnes/img-clip.nvim",
-    event = "VeryLazy",
-    opts = {
-        -- add options here
-        -- or leave it empty to use the default settings
+    {
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+            -- add options here
+            -- or leave it empty to use the default settings
+        },
+        keys = {
+            -- suggested keymap
+            { "<leader>pi", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
+        },
     },
-    keys = {
-        -- suggested keymap
-        { "<leader>pi", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
-    },
+    {
+        "kjuq/sixelview.nvim",
+        opts = {},
+    }
 }
