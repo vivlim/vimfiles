50a448971aab959a8fa03983085c9cf8ba29cba9
Viv <git@vvn.space>
2025-01-06 17:59:37 -0800

    show git status in margin

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -59,7 +59,6 @@ return {
             })
 
             require("telescope").load_extension("advanced_git_search")
-
         end,
         dependencies = {
             "nvim-telescope/telescope.nvim",
@@ -74,9 +73,70 @@ return {
     },
     {
         "sindrets/diffview.nvim",
+        lazy = false,
+        keys = {
+            { "<space>fgh", ":DiffviewFileHistory %<cr>", mode = "n", desc = "git file history diffview" },
+        },
+    },
+    {
+        "lewis6991/gitsigns.nvim",
         lazy = true,
         keys = {
-            {"<space>fgh", ":DiffviewFileHistory %<cr>", mode = "n", desc = "git file history diffview"},
+            {
+                '[c',
+                function()
+                    if vim.wo.diff then
+                        vim.cmd.normal({ '[c', bang = true })
+                    else
+                        require('gitsigns').nav_hunk('prev')
+                    end
+                end,
+                mode = 'n',
+                desc = 'prev hunk/change',
+            },
+            {
+                ']c',
+                function()
+                    if vim.wo.diff then
+                        vim.cmd.normal({ ']c', bang = true })
+                    else
+                        require('gitsigns').nav_hunk('next')
+                    end
+                end,
+                mode = 'n',
+                desc = 'next hunk/change',
+            },
         },
+        config = function()
+            require('gitsigns').setup {
+                on_attach = function(bufnr)
+                    local gitsigns = require('gitsigns')
+
+                    local function map(mode, l, r, opts)
+                        opts = opts or {}
+                        opts.buffer = bufnr
+                        vim.keymap.set(mode, l, r, opts)
+                    end
+
+                    -- Actions
+                    map('n', '<leader>hs', gitsigns.stage_hunk)
+                    map('n', '<leader>hr', gitsigns.reset_hunk)
+                    map('v', '<leader>hs', function() gitsigns.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
+                    map('v', '<leader>hr', function() gitsigns.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
+                    map('n', '<leader>hS', gitsigns.stage_buffer)
+                    map('n', '<leader>hu', gitsigns.undo_stage_hunk)
+                    map('n', '<leader>hR', gitsigns.reset_buffer)
+                    map('n', '<leader>hp', gitsigns.preview_hunk)
+                    map('n', '<leader>hb', function() gitsigns.blame_line { full = true } end)
+                    map('n', '<leader>tb', gitsigns.toggle_current_line_blame)
+                    map('n', '<leader>hd', gitsigns.diffthis)
+                    map('n', '<leader>hD', function() gitsigns.diffthis('~') end)
+                    map('n', '<leader>td', gitsigns.toggle_deleted)
+
+                    -- Text object
+                    map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+                end
+            }
+        end
     },
 }
