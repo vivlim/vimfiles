62b1477c13e5269588164c5dcefefa00e923048a
Viv <git@vvn.space>
2025-01-06 17:59:55 -0800

    additional latching, not sure if i want to keep it

--- lua/plugins/bindings.lua
+++ lua/plugins/bindings.lua
@@ -35,6 +35,18 @@ return {
         end,
         keys = {
             {
+                "<leader><leader>",
+                function()
+                    require('which-key').show({
+                        keys = "<leader>",
+                        loop = true,
+                    })
+                end,
+                mode = { "n", },
+                desc = "loop",
+
+            },
+            {
                 "<c-w><space>",
                 function()
                     require('which-key').show({
@@ -46,6 +58,79 @@ return {
                 desc = "loop",
 
             },
+            {
+                "]<space>",
+                function()
+                    require('which-key').show({
+                        keys = "]",
+                        loop = true,
+                    })
+                end,
+                mode = { "n", },
+                desc = "loop",
+
+            },
+            {
+                "[<space>",
+                function()
+                    require('which-key').show({
+                        keys = "[",
+                        loop = true,
+                    })
+                end,
+                mode = { "n", },
+                desc = "loop",
+            },
         },
     },
+    --    {
+    --        "anuvyklack/hydra.nvim",
+    --        lazy = false,
+    --        --        keys = {
+    --        --            {
+    --        --                "<space>D",
+    --        --                function()
+    --        --                    require('hydra').activate({
+    --        --                        name = "diff",
+    --        --                        config = {
+    --        --                            color = 'pink', -- persistent, see https://github.com/anuvyklack/hydra.nvim?tab=readme-ov-file#color
+    --        --                        },
+    --        --                        heads = {
+    --        --                            { 'q', nil, { nowait = true } }, -- exit
+    --        --                            { 'j',
+    --        --                        },
+    --        --
+    --        --                        hint = {
+    --        --                            type = "cmdline",
+    --        --                        },
+    --        --                    })
+    --        --                end,
+    --        --                mode = "n",
+    --        --                desc = "Diff mode",
+    --        --            }
+    --        --        },
+    --        config = function()
+    --            local Hydra = require("hydra")
+    --
+    --            Hydra({
+    --                name = 'Window',
+    --                mode = { 'n', 't', },
+    --                body = '<C-w>',
+    --                heads = {
+    --                    { 'h', },
+    --                    { 'j', },
+    --                    { 'k', },
+    --                    { 'l', },
+    --                    { 'H', },
+    --                    { 'J', },
+    --                    { 'K', },
+    --                    { 'L', },
+    --                    { '+', },
+    --                    { '-', },
+    --                    { '<', },
+    --                    { '>', },
+    --                }
+    --            })
+    --        end
+    --    },
 }
