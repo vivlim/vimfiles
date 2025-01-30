ca6e635c46ac12faa91508237db3f8e38fd2c02a
Viv Lim <git@vvn.space>
2025-01-29 22:47:55 -0800

    Revert coq

--- lua/plugins/semantic.lua
+++ lua/plugins/semantic.lua
@@ -132,20 +132,6 @@ return {
         event = { "BufReadPost", "BufWritePost", "BufNewFile" },
         -- TODO: replace much of this with https://www.lazyvim.org/plugins/lsp
         dependencies = {
-            -- main one
-            { "ms-jpq/coq_nvim", branch = "coq" },
-
-            -- 9000+ Snippets
-            { "ms-jpq/coq.artifacts", branch = "artifacts" },
-
-            -- lua & third party sources -- See https://github.com/ms-jpq/coq.thirdparty
-            -- Need to **configure separately**
-            { 'ms-jpq/coq.thirdparty', branch = "3p" },
-            -- - shell repl
-            -- - nvim lua api
-            -- - scientific calculator
-            -- - comment banner
-            -- - etc
             "williamboman/mason.nvim",
             "williamboman/mason-lspconfig.nvim",
         },
@@ -155,37 +141,6 @@ return {
             {"]d", function() vim.diagnostic.goto_next() end, mode = "n", desc = "next diagnostic"},
             {"<space>q", function() vim.diagnostic.setloclist() end, mode = "n", desc = "diagnostic loc list"},
         },
-        init = function()
-            vim.g.coq_settings = {
-                auto_start = true, -- if you want to start COQ at startup
-                -- Your COQ settings here
-                -- TODO: Tab to accept
-                -- or maybe not, i'll see if i can get used to it.
-                keymap = {
-                    recommended = false,
-                },
-            }
-
---            -- Keybindings
---            vim.api.nvim_set_keymap('i', '<Esc>', [[pumvisible() ? "\<C-e><Esc>" : "\<Esc>"]], { expr = true, silent = true })
---            vim.api.nvim_set_keymap('i', '<C-c>', [[pumvisible() ? "\<C-e><C-c>" : "\<C-c>"]], { expr = true, silent = true })
---            vim.api.nvim_set_keymap('i', '<BS>', [[pumvisible() ? "\<C-e><BS>" : "\<BS>"]], { expr = true, silent = true })
---            vim.api.nvim_set_keymap(
---                "i",
---                "<CR>",
---                [[pumvisible() ? (complete_info().selected == -1 ? "\<C-e><CR>" : "\<C-y>") : "\<CR>"]],
---                { expr = true, silent = true }
---            )
---            vim.api.nvim_set_keymap(
---                "i",
---                "<Tab>",
---                [[pumvisible() ? (complete_info().selected == -1 ? "\<C-e><CR>" : "\<CR>") : "\<Tab>"]],
---                { expr = true, silent = true }
---            )
---            vim.api.nvim_set_keymap('i', '<Tab>', [[pumvisible() ? "\<C-n>" : "\<Tab>"]], { expr = true, silent = true })
---            vim.api.nvim_set_keymap('i', '<S-Tab>', [[pumvisible() ? "\<C-p>" : "\<BS>"]], { expr = true, silent = true })
-
-        end,
         config = function()
             -- Install some lsps
             require("mason").setup()
@@ -195,29 +150,43 @@ return {
                 -- and will be called for each installed server that doesn't have
                 -- a dedicated handler.
                 function (server_name) -- default handler (optional)
+                    local capabilities =
+                        require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
                     require("lspconfig")[server_name].setup({
                         on_attach = lsp_on_attach,
+                        capabilities = capabilities,
                     })
                 end,
                 -- Next, you can provide a dedicated handler for specific servers.
                 ["nil_ls"] = function ()
+                    local capabilities =
+                        require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
                     require("lspconfig").nil_ls.setup({
                         on_attach = lsp_on_attach,
+                        capabilities = capabilities,
                     })
                 end,
                 ["elixirls"] = function ()
+                    local capabilities =
+                        require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
                     require("lspconfig").elixirls.setup({
                         on_attach = lsp_on_attach,
+                        capabilities = capabilities,
                         cmd = { "elixir-ls" }, -- This should be on the path if the project has a nix flake devshell & direnv configured.
                     })
                 end,
                 ["rust_analyzer"] = function ()
+                    local capabilities =
+                        require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
                     require("lspconfig").rust_analyzer.setup({
                         on_attach = lsp_on_attach,
+                        capabilities = capabilities,
                     })
                 end,
             }
 
+            local capabilities =
+                require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
             require("lspconfig").lua_ls.setup({
                 on_attach = lsp_on_attach,
                 capabilities = capabilities,
